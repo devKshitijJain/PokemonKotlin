345a1e9b7841a1edcac6327c8bf774e220fe339e
Kshitij Jain <007techfreeks@gmail.com>
2018-03-06 14:53:32 +0530

    Remove AutoValue

--- app/src/main/java/in/co/kshitijjain/pokemonkotlin/pokemon/viewstate/PokemonViewState.kt
+++ app/src/main/java/in/co/kshitijjain/pokemonkotlin/pokemon/viewstate/PokemonViewState.kt
@@ -1,52 +1,26 @@
 package `in`.co.kshitijjain.pokemonkotlin.pokemon.viewstate
 
-import com.google.auto.value.AutoValue
-
 abstract class PokemonViewState {
 
     abstract fun results(): List<ResultViewState>
 
     abstract fun accept(visitor: Visitor)
 
-    open fun toLoading(): Loading {
-        return Builder(this)
-                .buildLoading()
-    }
-
-    fun toError(type: Error.Type, cause: Throwable): Error {
-        return Builder(this)
-                .buildError(type, cause)
-    }
-
-    @AutoValue
     abstract class Loading : PokemonViewState() {
-
         override fun accept(visitor: Visitor) {
             visitor.visit(this)
         }
     }
 
-    @AutoValue
     abstract class Idle : PokemonViewState() {
-
-        override fun toLoading(): Loading {
-            return Builder(this)
-                    .buildLoading()
-        }
-
         override fun accept(visitor: Visitor) {
             visitor.visit(this)
         }
     }
 
-    @AutoValue
     abstract class Error : PokemonViewState() {
 
         abstract fun cause(): Throwable
-
-        override fun accept(visitor: Visitor) {
-            visitor.visit(this)
-        }
 
         abstract fun type(): Type
 
@@ -55,34 +29,37 @@ abstract class PokemonViewState {
             CONNECTION,
             UNKNOWN
         }
-    }
 
-    private class Builder internal constructor(private val results: List<ResultViewState>) {
-
-        internal constructor(pokemonViewState: PokemonViewState)
-                : this(pokemonViewState.results())
-
-        internal fun buildIdle(): Idle {
-            return AutoValue_PokemonViewState_Idle(results)
-        }
-
-        internal fun buildLoading(): Loading {
-            return AutoValue_PokemonViewState_Loading(results)
-        }
-
-        internal fun buildError(type: Error.Type, cause: Throwable)
-                : Error {
-            return AutoValue_PokemonViewState_Error(results, cause, type)
+        override fun accept(visitor: Visitor) {
+            visitor.visit(this)
         }
     }
 
     interface Visitor {
 
-        fun visit(idle: Idle)
-
         fun visit(loading: Loading)
 
+        fun visit(idle: Idle)
+
         fun visit(error: Error)
+    }
+
+    private class Builder internal constructor(private val results: List<ResultViewState>) {
+
+        constructor(pokemonViewState: PokemonViewState)
+                : this(pokemonViewState.results())
+
+        internal fun buildIdle(): Idle {
+            return IdlePokemonViewState(results)
+        }
+
+        internal fun buildLoading(): Loading {
+            return LoadingPokemonViewState(results)
+        }
+
+        internal fun buildError(type: Error.Type, cause: Throwable): Error {
+            return ErrorPokemonViewState(results, cause, type)
+        }
     }
 
     companion object {
@@ -94,6 +71,44 @@ abstract class PokemonViewState {
 
         fun empty(): PokemonViewState {
             return create(emptyList())
+        }
+    }
+
+    fun toLoading(): Loading {
+        return PokemonViewState.Builder(this)
+                .buildLoading()
+    }
+
+    fun toError(type: Error.Type, cause: Throwable): Error {
+        return PokemonViewState.Builder(this)
+                .buildError(type, cause)
+    }
+
+    data class LoadingPokemonViewState(private val results: List<ResultViewState>) : Loading() {
+        override fun results(): List<ResultViewState> {
+            return results
+        }
+    }
+
+    data class IdlePokemonViewState(private val results: List<ResultViewState>) : Idle() {
+        override fun results(): List<ResultViewState> {
+            return results
+        }
+    }
+
+    data class ErrorPokemonViewState(private val results: List<ResultViewState>,
+                                     private val cause: Throwable,
+                                     private val type: Type) : Error() {
+        override fun results(): List<ResultViewState> {
+            return results
+        }
+
+        override fun cause(): Throwable {
+            return cause
+        }
+
+        override fun type(): Type {
+            return type
         }
     }
 }
